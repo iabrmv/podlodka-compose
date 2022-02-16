bdd7bcf383c065d6330505edafb2fa922aba0084
iabrmv <vancir@gmail.com>
2022-02-16 23:29:35 +0300

    stars implemented

--- app/src/main/java/com/iabrmv/podlodkacompose/ui/theme/Type.kt
+++ app/src/main/java/com/iabrmv/podlodkacompose/ui/theme/Type.kt
@@ -2,14 +2,23 @@ package com.iabrmv.podlodkacompose.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.iabrmv.podlodkacompose.R
 
 // Set of Material typography styles to start with
+
+val skModernistFamily = FontFamily(
+    Font(R.font.sk_modernist_regular, FontWeight.Normal),
+    Font(R.font.sk_modernist_bold, FontWeight.Bold),
+    Font(R.font.sk_modernist_mono, FontWeight.Medium)
+)
+
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = skModernistFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
